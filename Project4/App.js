7f6f827cdb9562b44c2126cea777ed78ce14cfc5
shahriar-math1364 <shahriar-math1364@users.noreply.github.com>
2023-10-16 20:46:57 -0400

    Create App.js

---
+++ Project4/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DragAndDropArea from './components/DragAndDropArea';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Potato Leaf Image Inference Testpad</h1>
+      <DragAndDropArea />
+    </div>
+  );
+}
+
+export default App;
