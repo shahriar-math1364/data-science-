8984340a6a9fbc34d17959ff69a9286f51f2a7b7
shahriar-math1364 <shahriar-math1364@users.noreply.github.com>
2023-10-16 20:49:32 -0400

    Create main.py

---
+++ Project4/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from PIL import Image
+import io
+import numpy as np
+from app.model import recognize_image
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+def resize_image(image_data, target_size=(256, 256)):
+    image = Image.open(io.BytesIO(image_data))
+    image = image.resize(target_size)
+    return np.array(image)
+
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile):
+    if file:
+        image = await file.read()
+
+        image = resize_image(image)
+
+        result = recognize_image(image)
+        c= result['class']
+        confidence=result['confidence']
+        return f"Result: Class:{result['class']} Confidence:{result['confidence']}"
+    return {"result": "No image provided"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000)
